8941b82c8a9d99a68972fa50ed35b302acfd5bb8
menyagah <menyagah27@gmail.com>
2021-09-24 16:41:26 +0300

    Change mode to development

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
