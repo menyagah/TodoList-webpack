4127b996b3ec3d5f07c2ef503f60bbddcc326392
menyagah <menyagah27@gmail.com>
2021-09-25 21:59:47 +0300

    Add an edit file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style/styles.css';
 import addTodo from './eventListners.js';
 import displayTodos from './displayItems.js';
 import attachInputEventListeners from './attachCheckListener.js';
-import clearAllBtn from './clearAll.js'
+import clearAllBtn from './clearAll.js';
 
 displayTodos();
 addTodo();
