a2417a37d675612e0ddfd3185cb36d667b6038d2
menyagah <menyagah27@gmail.com>
2021-09-25 07:31:07 +0300

    Add dynamically rendered data in js and style it with icons

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import './style/styles.css';
 
-console.log('it is working');
+const data = document.querySelector('.data');
+const todoList = [{
+    description: 'wash the dishes',
+    completed: false,
+    id: 1,
+  },
+  {
+    description: 'complete the dishes',
+    completed: false,
+    id: 2,
+  },
+  {
+    description: 'complete To Do list project',
+    completed: false,
+    id: 3,
+  },
+  ];
+
+for (let todos of todoList){
+    const div = document.createElement('div');
+    div.classList.add('d-height');
+    div.classList.add('todo-cont');
+    div.innerHTML = `<li><input type="checkbox" class="check-box"   id="check">${todos.description}</li>
+    <div class="delete-icon"></div>`
+    data.appendChild(div)
+}
